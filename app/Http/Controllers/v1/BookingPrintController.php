a04b4cf8787baed1330b268b29d5fe1b5e3ea1a5
Prgnesh <vishalkotak200@gmail.com>
2021-12-08 19:24:40 +0530

    BookingPrint Api Updation 08-12-2021

--- app/Http/Controllers/v1/BookingPrintController.php
+++ app/Http/Controllers/v1/BookingPrintController.php
@@ -53,7 +53,7 @@ class BookingPrintController extends Controller
                 ->with('original_manufacturer:id,company_name')
                 ->with('supplier:id,company_name')
                 ->with('sample_data:id,booking_id,product_id,batch_no as lot_batch_no,batch_size_qty_rec,sample_quantity as sample_qty_rec,sample_condition as condition_of_sample', 'sample_data.product_data:id,product_name as name_of_sample,product_generic,pharmacopeia_id,generic_product_id,sample_description', 'sample_data.product_data.generic_product_data:id,product_name as generic_name', 'sample_data.product_data.pharmacopiea_data:id,pharmacopeia_name')
-                ->with('tests_data:id,booking_id,test_name as test_parameter,label_claim,result,max_limit,product_details,test_date_time as date_of_performance_test,method as method_used,approved,approval_date_time,parent,parent_child,p_sr_no', 'tests_data.parent_data:id,machine_name as parent_name')
+                ->with('tests_data:id,booking_id,test_name as test_parameter,label_claim,result,max_limit,product_details,test_date_time as date_of_performance_test,method as method_used,approved,approval_date_time,parent,parent_child,p_sr_no,approval_date_time', 'tests_data.parent_data:id,machine_name as parent_name')
                 ->with('latest_test_date_time:id,booking_id,test_date_time')
                 ->where('id', $id)
                 ->get()->toarray();
@@ -61,21 +61,13 @@ class BookingPrintController extends Controller
             $rules = [
                 'tests_data.*.max_limit' => 'required_without:sample_data.*.product_data.sample_description',
                 'sample_data.*.product_data.sample_description' => 'required_without:tests_data.*.max_limit',
+                'tests_data.*.result' => 'required',
             ];
             $msg = [
                 'tests_data.*.max_limit.required_without' => 'The Tests Data Max Limit field is Required When Sample Description is Not Present',
                 'sample_data.*.product_data.sample_description.required_without' => 'The Sample Data Sample Description field is Required When Tests Data Max Limit is Not Present',
+                'tests_data.*.result.required' => "Test Result Must Be Required For COA/ROA."
             ];
-            if ($type == "Nabl_Print") {
-                $rules['tests_data.*.date_of_performance_test'] = "required";
-                $msg['tests_data.*.date_of_performance_test.required'] = "All The Dates Of Performance Tests are Required When Print is NABL Print.";
-            }
-            $validator = Validator::make($data[0], $rules, $msg);
-
-            if ($validator->fails()) {
-                $data = array();
-                return Helper::response($validator->errors()->all(), Response::HTTP_OK, false, $data);
-            }
 
             if (!empty($data[0]['tests_data'])) {
                 $state_id = $data[0]['customer_data']['customer_contact_data']['state'];
@@ -109,13 +101,27 @@ class BookingPrintController extends Controller
                         $can_roa_print = 0;
                     }
                 }
-                if ($type == "Roa_print") {
+                if ($type == "Roa_Print" || $type == "Check_Roa") {
+                    $validator = Validator::make($data[0], $rules, $msg);
+
+                    if ($validator->fails()) {
+                        $data = array();
+                        return Helper::response($validator->errors()->all(), Response::HTTP_OK, false, $data);
+                    }
                     if ($can_roa_print == 1) {
                         if ($print_allows->roa_print_count != 1) {
-                            $print_allows->update(array('roa_print_count' => 1, 'roa_print' => $type));
-                            return Helper::response("Roa Print Generated Successfully", Response::HTTP_OK, true, $data);
+                            if ($type == "Check_Roa") {
+                                return Helper::response("You Can Proceed For Print, Show, PDF", Response::HTTP_OK, true);
+                            } else {
+                                $print_allows->update(array('roa_print_count' => 1, 'roa_print' => $type));
+                                return Helper::response("Roa Print Generated Successfully", Response::HTTP_OK, true, $data);
+                            }
                         } else {
-                            return Helper::response("Roa Print is Already Generated", Response::HTTP_OK, true);
+                            if ($type == "Check_Roa") {
+                                return Helper::response("Roa Print is Already Generated But You Can Proceed For Show, PDF Only.", Response::HTTP_OK, true);
+                            } else {
+                                return Helper::response("Roa Print is Already Generated", Response::HTTP_OK, false);
+                            }
                         }
                     } else {
                         $msg = "All the Tests Can Be 'Assigned' or 'ForApproval' or 'Approved' & Can not be 'Pending' For Roa Print.";
@@ -123,14 +129,11 @@ class BookingPrintController extends Controller
                     }
                 } else {
                     if ($can_coa_print == 1) {
-                        $rules = [
-                            'tests_data.*.result' => 'required',
-                            'tests_data.*.approval_date_time' => 'required',
-                        ];
-                        $msg = [
-                            'tests_data.*.result.required' => 'Tests Results Must Be Required For' . $type . '',
-                            'tests_data.*.approval_date_time.required' => 'Tests Approval Date Time Must Be Required For' . $type . '',
-                        ];
+                        $rules['tests_data.*.approval_date_time'] = "required";
+                        $rules['tests_data.*.date_of_performance_test'] = "required";
+                        $msg["tests_data.*.approval_date_time.required"] = "Test Approval Date Time Must Be Required.";
+                        $msg["tests_data.*.date_of_performance_test.required"] = "Date Of Performance of the Test are Required.";
+
                         $validator = Validator::make($data[0], $rules, $msg);
 
                         if ($validator->fails()) {
@@ -139,13 +142,20 @@ class BookingPrintController extends Controller
                         }
 
                         if (($print_allows->coa_print == null || $print_allows->coa_print == '') && $print_allows->coa_print_count == 0) {
-
-                            $print_allows->update(array('coa_print' => $type, 'coa_print_count' => 1, 'booking_type' => 'Report'));
-                            return Helper::response($type . " Generated Successfully", Response::HTTP_OK, true, $data);
+                            if ($type == "Check_Coa") {
+                                return Helper::response("You Can Proceed For Coa, Nabl, Ayush Print, Show, PDF", Response::HTTP_OK, true);
+                            } else {
+                                $print_allows->update(array('coa_print' => $type, 'coa_print_count' => 1, 'booking_type' => 'Report'));
+                                return Helper::response($type . " Generated Successfully", Response::HTTP_OK, true, $data);
+                            }
                         } else {
-
-                            $msg = "Print is Already Generated.";
-                            return Helper::response($msg, Response::HTTP_OK, false);
+                            if ($type == "Check_Coa") {
+                                $msg = "Coa Print is Already Generated But You Can Preceed For Show, PDF Only.";
+                                return Helper::response($msg, Response::HTTP_OK, true);
+                            } else {
+                                $msg = $type . " Print is Already Generated.";
+                                return Helper::response($msg, Response::HTTP_OK, false);
+                            }
                         }
                     } else {
                         $msg = "All the Tests Must Be 'Approved' For Coa Print.";
@@ -222,17 +232,18 @@ class BookingPrintController extends Controller
                 ->with('original_manufacturer:id,company_name')
                 ->with('supplier:id,company_name')
                 ->with('sample_data:id,booking_id,product_id,batch_no as lot_batch_no,batch_size_qty_rec,sample_quantity as sample_qty_rec,sample_condition as condition_of_sample', 'sample_data.product_data:id,product_name as name_of_sample,product_generic,pharmacopeia_id,generic_product_id,sample_description', 'sample_data.product_data.generic_product_data:id,product_name as generic_name', 'sample_data.product_data.pharmacopiea_data:id,pharmacopeia_name')
-                ->with('tests_data:id,booking_id,test_name as test_parameter,label_claim,result,max_limit,product_details,test_date_time as date_of_performance_test,method as method_used,approved,parent,parent_child,p_sr_no', 'tests_data.parent_data:id,machine_name as parent_name')
+                ->with('tests_data:id,booking_id,test_name as test_parameter,label_claim,result,max_limit,product_details,test_date_time as date_of_performance_test,method as method_used,approved,parent,parent_child,p_sr_no,approval_date_time', 'tests_data.parent_data:id,machine_name as parent_name')
+                ->with('latest_test_date_time:id,booking_id,test_date_time')
                 ->where('id', $id)
                 ->get()->toarray();
 
             $rules = [
-                // 'tests_data.*.result' => 'required',
-                // 'tests_data.*.approval_date_time' => 'required',
+                'tests_data.*.result' => 'required',
+                'tests_data.*.approval_date_time' => 'required',
             ];
             $msg = [
-                // 'tests_data.*.result.required' => 'Tests Result Must Be Required.',
-                // 'tests_data.*.approval_date_time.required' => 'Tests Approval Date Time Must Be Required.',
+                'tests_data.*.result.required' => 'Tests Result Must Be Required.',
+                'tests_data.*.approval_date_time.required' => 'Tests Approval Date Time Must Be Required.',
             ];
             $validator = Validator::make($data[0], $rules, $msg);
 
